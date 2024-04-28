f62069afee71823d7a0eec19e1c2f772a98b3a4c
Totktomolodes <maxim-programmer@mail.ru>
2024-04-28 16:21:54 +0300

    прогресс использования фрагментов

--- app/src/main/java/com/example/final_book_explorer_project/activities/MainActivity2.java
+++ app/src/main/java/com/example/final_book_explorer_project/activities/MainActivity2.java
@@ -1,16 +1,46 @@
 package com.example.final_book_explorer_project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.example.final_book_explorer_project.R;
+import com.example.final_book_explorer_project.fragments.installer.InstallerFragment;
+
+import java.time.chrono.IsoChronology;
 
 public class MainActivity2 extends AppCompatActivity {
+    private Button catalog, mycatalog, installer, user_profile;
+    private FrameLayout Frame_transition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        catalog = findViewById(R.id.catalog);
+        mycatalog = findViewById(R.id.mycatalog);
+        installer = findViewById(R.id.installer);
+        user_profile = findViewById(R.id.user_profile);
+        Frame_transition = findViewById(R.id.Frame_transition);
+
+        setInstallerFragment();
+        catalog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setInstallerFragment();
+            }
+        });
+
+    }
+
+    private void setInstallerFragment() {
+        InstallerFragment installerFragment = new InstallerFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.Frame_transition, installerFragment);
     }
 }
---
+++ app/src/main/java/com/example/final_book_explorer_project/fragments/user_profile/Profile_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.final_book_explorer_project.fragments.user_profile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.final_book_explorer_project.R;
+
+public class Profile_Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_, container, false);
+    }
+}
